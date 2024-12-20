f667343fc9e95c2d7265abbd89a42407bdda8ccf
nitesw <apedos2007@gmail.com>
2024-12-20 03:48:25 +0100

    Add basic routes

--- online-shop-frontend/src/App.tsx
+++ online-shop-frontend/src/App.tsx
@@ -8,7 +8,11 @@ function App() {
     <MantineProvider>
         <Routes>
             <Route path="/" element={<div>Home page</div>}/>
+            <Route path="/community" element={<div>Community page</div>}/>
+            <Route path="/library" element={<div>Library page</div>}/>
+            <Route path="/profile" element={<div>Profile page</div>}/>
             <Route path="/support" element={<div>Support page</div>}/>
+            <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
     </MantineProvider>
   )
