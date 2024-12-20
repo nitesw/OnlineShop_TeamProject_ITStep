a89bbb4f3872a237a6466f967f1b2e1fb63027f8
nitesw <apedos2007@gmail.com>
2024-12-20 03:26:40 +0100

    Implement react-router-dom

--- online-shop-frontend/src/App.tsx
+++ online-shop-frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import '@mantine/core/styles.css';
 import {MantineProvider} from "@mantine/core";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <MantineProvider>
-      <div>Hello world!</div>
+        <Routes>
+            <Route path="/" element={<div>Home page</div>}/>
+            <Route path="/support" element={<div>Support page</div>}/>
+        </Routes>
     </MantineProvider>
   )
 }
